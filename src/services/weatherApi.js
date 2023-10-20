68d922a21e721a4a063581afc2e56fd1be11ae5d
Antonio Martin <amartindev02@gmail.com>
2023-10-20 14:17:53 -0500

    env file

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,5 +1,5 @@
 const urlBase = "https://api.openweathermap.org/data/2.5/weather"
-const API_KEY = 'e4669f52341e7fa19bcbec3d3bb92acd'
+const API_KEY = import.meta.env.VITE_WEATHER_APP_API_KEY
 
 const fetchClima = async (ciudad) => {
     try{
